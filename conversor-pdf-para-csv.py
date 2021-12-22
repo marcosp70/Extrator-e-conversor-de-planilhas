74339c290528a44eb4510b8892db82d204bee6a3
Marcos <marcossilvabh70@gmail.com>
2021-12-22 17:25:56 -0300

    codigo em poo

--- conversor-pdf-para-csv.py
+++ conversor-pdf-para-csv.py
@@ -1,33 +1,41 @@
-# restrições do programa: tenha somente na pasta os arquivos pdfs! Além disso, não coloque, no nome dos arquivos um ".", deixe somente o "." do ".pdf"
+# Restrições do programa: tenha somente na pasta os arquivos pdfs! Além disso, não coloque, no nome dos arquivos um ".", deixe somente o "." do ".pdf"
 
 from os import chdir, getcwd, listdir
 import tabula
 
-cam = input('Digite o caminho: ')
+class Conversor:
 
-chdir(cam)
-print(getcwd())
+    def __init__(self,diretorio: str) -> None:
+        self.lista_pdf = []
+        self.lista_csv = []
+        self.diretorio = diretorio
 
-lista = []  # lista em pdf
-lista2 = []  # lista em csv
+    def converter(self):
+        self.__lista_arquivos_pdf()
+        self.__lista_arquivos_clv()
+        for i in range(0, len(self.lista_csv)):  # conversão de cada lista na pasta de pdf para csv
+            print(self.lista_pdf[i])  # arquivo que será convertido
+            tabula.convert_into(self.lista_pdf[i], self.lista_csv[i], output_format="csv", pages='all')  # conversão
+            print("{}\n-------------------------".format(self.lista_csv[i]))  # arquivo convertido
 
-for c in listdir():  # coleta o nome de todos os arquivos da pasta para a lista
-    lista.append(c)
+    def __lista_arquivos_pdf(self):   #Método para definir todos os arquivos .pdf que serão convertidos da pasta
+        chdir(self.diretorio)
+        print("\nConvertendo arquivos .pdf do diretório {}\n------------------------------------------------------\n".format(getcwd()))
 
-lista2 = lista[
-         :]  # pega os mesmos arquivos de uma lista e passa para outra, assim podemos mudar o "final" do nome do arquivo (.pdf para .csv)
+        for c in listdir():  # coleta o nome de todos os arquivos da pasta para a lista
+            self.lista_pdf.append(c)
 
-for i in range(0,
-               len(lista2)):  # for com o intuito de tirar o ".pdf" e colocar ".csv" para poder exercutar no método final
-    mudar = lista2[i].find(".pdf")
-    nova_palavra = lista2[i][:mudar] + "_convertido"
-    nova_palavra = nova_palavra + ".csv"
-    lista2[i] = nova_palavra
+    def __lista_arquivos_clv(self): #Método para obter o novo nome dos arquivos convertidos para .csv
 
-print(lista)  # printa da lista em pdf
-print(lista2)  # print da lista em csv
+        self.lista_csv = self.lista_pdf[:]
 
-for i in range(0, len(lista2)):  # conversão de cada lista na pasta de pdf para csv
-    print(lista[i])  # arquivo que será convertido
-    print(lista2[i])  # arquivo convertido
-    tabula.convert_into(lista[i], lista2[i], output_format="csv", pages='all')  # conversão
+        for i in range(0, len(self.lista_csv)):  # for com o intuito de tirar o ".pdf" e colocar ".csv" para poder exercutar no método final
+            mudar = self.lista_csv[i].find(".pdf")
+            nova_palavra = self.lista_csv[i][:mudar] + "_convertido.csv"
+            self.lista_csv[i] = nova_palavra
+
+diretorio = input('Digite o caminho da pasta: ')
+run = Conversor(diretorio)
+run.converter()
+print("Conversão finalizada!")
+
